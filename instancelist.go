ee9830d83693068ef56dddae2879fb3010c6d34b
M. Filio <mdfilio@gmail.com>
2016-10-17 15:44:59 -0500

    Add Ohio. Update Readme

--- instancelist.go
+++ instancelist.go
@@ -93,16 +93,17 @@ func main() {
 
 	awsregions := map[int][]string{
 		0:  {"us-east-1", "N. Virginia"},
-		1:  {"us-west-1", "N. California"},
-		2:  {"us-west-2", "Oregon"},
-		3:  {"eu-west-1", "Ireland"},
-		4:  {"eu-central-1", "Frankfurt"},
-		5:  {"ap-southeast-1", "Singapore"},
-		6:  {"ap-southeast-2", "Sydney"},
-		7:  {"ap-northeast-1", "Tokyo"},
-		8:  {"ap-northeast-2", "Seoul"},
-		9:  {"sa-east-1", "São Paulo"},
-		10: {"ap-south-1", "Mumbai"},
+		1:  {"us-east-2", "Ohio"},
+		2:  {"us-west-1", "N. California"},
+		3:  {"us-west-2", "Oregon"},
+		4:  {"eu-west-1", "Ireland"},
+		5:  {"eu-central-1", "Frankfurt"},
+		6:  {"ap-southeast-1", "Singapore"},
+		7:  {"ap-southeast-2", "Sydney"},
+		8:  {"ap-northeast-1", "Tokyo"},
+		9:  {"ap-northeast-2", "Seoul"},
+		10: {"sa-east-1", "São Paulo"},
+		11: {"ap-south-1", "Mumbai"},
 	}
 	goGroup := new(sync.WaitGroup)
 	defer goGroup.Wait()
